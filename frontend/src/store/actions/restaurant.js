18bb8b8dabe88464c7db58908d3f216e29b1e6c2
cm399 <ideagood64@gmail.com>
2022-09-10 13:56:46 -0400

    add resources

--- frontend/src/store/actions/restaurant.js
+++ frontend/src/store/actions/restaurant.js
@@ -1,34 +1,34 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
 import axios from "../../axios";
 
-const compare = ( a, b ) => {
-    if ( a.rating < b.rating ){
-        return -1;
-    }
-    if ( a.rating > b.rating ){
-        return 1;
-    }
-    return 0;
-}
+const compare = (a, b) => {
+  if (a.rating < b.rating) {
+    return -1;
+  }
+  if (a.rating > b.rating) {
+    return 1;
+  }
+  return 0;
+};
 
 export const fetchRestaurants = () => {
-    return dispatch => {
-        dispatch({type: actionTypes.FETCH_START});
-        axios.get('/restaurants')
-            .then(response => {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_START });
+    axios
+      .get("/restaurants")
+      .then((response) => {
+        const restaurants = response.data;
 
-                const restaurants = response.data;
-
-                dispatch({
-                    type: actionTypes.FETCH_RESTAURANTS_SUCCESS,
-                    restaurants: restaurants.sort(compare).reverse()
-                });
-            })
-            .catch(error => {
-                dispatch({
-                    type: actionTypes.FETCH_ERROR,
-                    error: error
-                });
-            });
-    }
-}
+        dispatch({
+          type: actionTypes.FETCH_RESTAURANTS_SUCCESS,
+          restaurants: restaurants.sort(compare).reverse(),
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: actionTypes.FETCH_ERROR,
+          error: error,
+        });
+      });
+  };
+};
